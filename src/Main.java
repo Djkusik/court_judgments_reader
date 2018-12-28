0d77225fdf83950333fee84243a2cf295ca426c7
Djkusik <djkusin@gmail.com>
2018-12-28 07:55:01 +0100

    Testing terminal

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import shell.Shell;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Shell shell;
+        if (args.length == 2)
+            shell = new Shell(args[0], args[1]);
+        else if (args.length == 1)
+            shell = new Shell(args[0]);
+        else {
+            System.out.println("You have to give at least one argument with path to judgment files, and one optional for logging!");
+            return;
+        }
+        shell.init();
+        shell.run();
     }
 }
--- src/shell/Shell.java
+++ src/shell/Shell.java
@@ -1,9 +1,14 @@
 package shell;
 
-import org.jline.reader.Completer;
-import org.jline.reader.LineReaderBuilder;
+import model.Judgment;
+import finder.Collector;
+import org.jline.reader.*;
+import org.jline.reader.impl.completer.StringsCompleter;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
+import statistics.JudgesStatistics;
+import statistics.JudgmentStatistics;
+import parser.Parser;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -11,9 +16,26 @@ import java.util.List;
 public class Shell {
 
     private String[] optionsList;
+    private Judgment judgment;
+    private Parser parser;
+    private JudgesStatistics judgesStats;
+    private JudgmentStatistics judgmentsStats;
+    private Collector collector;
+
+    public Shell (String pathToParse) {
+        this.judgment = new Judgment();
+        this.parser = new Parser();
+        judgment = parser.parse(pathToParse);
+    }
+
+    public Shell (String pathToParse, String pathToSaveLog) {
+        this.judgment = new Judgment();
+        this.parser = new Parser();
+        judgment = parser.parse(pathToParse);
+    }
 
     public void init() {
-        optionsList = new String[] [ "1", "2", "3", "4", "5", "6", "7", "8" ];
+        optionsList = new String[] { "help", "rubrum", "content", "judge", "judges", "months", "courts", "regulations", "jury" };
     }
 
     public void run() {
@@ -21,11 +43,44 @@ public class Shell {
                 .system(true)
                 .build();
 
-        LineReaderBuilder readerBuilder = LineReaderBuilder.builder();
         List<Completer> completors = new LinkedList<Completer>();
+        completors.add(new StringsCompleter(optionsList));
+
+        LineReaderBuilder reader = LineReaderBuilder.builder()
+                .terminal(terminal)
+                .completer(completors)
+                .build();
+
+        String line;
+        String prompt = "$> ";
+
+        while((line = readLine(reader, "")) != null) {
+
+        }
+
+    }
+
+    private String readLine(LineReader reader, String promtMessage) {
+        try {
+            String line = reader.readLine(promtMessage + "\nshell> ");
+            return line.trim();
+        }
+        catch (UserInterruptException e) {
+            return null;
+        }
+        catch (EndOfFileException e) {
+            return null;
+        }
     }
 
     private void printHelp() {
-        System.out.println()
+        System.out.println("rubrum - wyświetlenie metryki jednego lub wielu orzeczeń, na podstawie sygnatury");
+        System.out.println("content - wyświetlenie uzasadnienia dla wybranej sygnatury");
+        System.out.println("judge - wyświetlenie liczby orzeczeń dla wybranego sędziego");
+        System.out.println("judges - wyświetlenie 10 sędziów, którzy wydali najwięcej orzeczeń");
+        System.out.println("months - wyświetlenie liczby orzeczeń w poszczególnych miesiącach (rozkład statystyczny)");
+        System.out.println("courts - wyświetlenie liczby orzeczeń ze względu na typ sądu (rozkład statystyczny)");
+        System.out.println("regulations - wyświetlenie 10 najczęściej przywoływanych ustaw");
+        System.out.println("jury - wyświetlenie liczby spraw przypadających na określony skład sędziowski (określoną liczbę sędziów)");
     }
 }
