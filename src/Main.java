a3695ff81cbf0d419f46f48eaca0fdbeb44d7d0c
Djkusik <djkusin@gmail.com>
2018-12-28 22:20:16 +0100

    Fully working terminal with correct main

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,27 @@
+import parser.Parser;
 import shell.Shell;
+
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-        Shell shell;
-        if (args.length == 2)
-            shell = new Shell(args[0], args[1]);
-        else if (args.length == 1)
-            shell = new Shell(args[0]);
-        else {
-            System.out.println("You have to give at least one argument with path to judgment files, and one optional for logging!");
-            return;
-        }
-        shell.init();
-        shell.run();
+        Shell.clearScreen();
+
+        if (args.length > 0) {
+            try {
+                Parser parser = new Parser();
+                if (args.length == 2) {
+                    parser = parser.parse(args[0], args[1]);
+                } else {
+                    parser = parser.parse(args[0], null);
+                }
+                Shell shell = new Shell(parser);
+                shell.run();
+            } catch (IOException err) {
+                System.out.println("Podana ścieżka jest błędna");
+            }
+        } else
+            System.out.println("Poprawną formą jest podanie ścieżki do folderu ze źródłem oraz opcjonalny plik z zapisem logu użytkowania");
     }
 }
--- src/shell/Shell.java
+++ src/shell/Shell.java
@@ -1,86 +1,55 @@
 package shell;
 
-import model.Judgment;
-import finder.Collector;
+import finder.Commands;
 import org.jline.reader.*;
-import org.jline.reader.impl.completer.StringsCompleter;
+import org.jline.reader.impl.completer.EnumCompleter;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
-import statistics.JudgesStatistics;
-import statistics.JudgmentStatistics;
 import parser.Parser;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.io.IOException;
 
 public class Shell {
 
-    private String[] optionsList;
-    private Judgment judgment;
     private Parser parser;
-    private JudgesStatistics judgesStats;
-    private JudgmentStatistics judgmentsStats;
-    private Collector collector;
 
-    public Shell (String pathToParse) {
-        this.judgment = new Judgment();
-        this.parser = new Parser();
-        judgment = parser.parse(pathToParse);
+    public Shell(Parser parser) {
+        this.parser = parser;
     }
 
-    public Shell (String pathToParse, String pathToSaveLog) {
-        this.judgment = new Judgment();
-        this.parser = new Parser();
-        judgment = parser.parse(pathToParse);
-    }
+    public void run() throws IOException {
+        this.clearScreen();
 
-    public void init() {
-        optionsList = new String[] { "help", "rubrum", "content", "judge", "judges", "months", "courts", "regulations", "jury" };
-    }
-
-    public void run() {
         Terminal terminal = TerminalBuilder.builder()
                 .system(true)
+                .name("Court_Judgments_Reader")
                 .build();
 
-        List<Completer> completors = new LinkedList<Completer>();
-        completors.add(new StringsCompleter(optionsList));
+        Completer completors = new EnumCompleter(Commands.class);
 
-        LineReaderBuilder reader = LineReaderBuilder.builder()
+        LineReader lineReader = LineReaderBuilder.builder()
                 .terminal(terminal)
                 .completer(completors)
                 .build();
+        while (true) {
+            String input = lineReader.readLine("Twoja komenda: ");
+            try {
+                if(parser.getSaveToFile() != null) {
+                    Commands.saveToFile(parser.getSaveToFile(), "Wpisana komenda: " + input);
+                }
 
-        String line;
-        String prompt = "$> ";
-
-        while((line = readLine(reader, "")) != null) {
-
-        }
-
-    }
-
-    private String readLine(LineReader reader, String promtMessage) {
-        try {
-            String line = reader.readLine(promtMessage + "\nshell> ");
-            return line.trim();
-        }
-        catch (UserInterruptException e) {
-            return null;
-        }
-        catch (EndOfFileException e) {
-            return null;
+                Commands.valueOf(input.toLowerCase()).execute(parser);
+            } catch (IllegalArgumentException err) {
+                if(parser.getSaveToFile() != null) {
+                    Commands.saveToFile(parser.getSaveToFile(), "Nieobsługiwana komenda");
+                }
+                System.out.println("Nieobsługiwana komenda");
+            }
         }
     }
 
-    private void printHelp() {
-        System.out.println("rubrum - wyświetlenie metryki jednego lub wielu orzeczeń, na podstawie sygnatury");
-        System.out.println("content - wyświetlenie uzasadnienia dla wybranej sygnatury");
-        System.out.println("judge - wyświetlenie liczby orzeczeń dla wybranego sędziego");
-        System.out.println("judges - wyświetlenie 10 sędziów, którzy wydali najwięcej orzeczeń");
-        System.out.println("months - wyświetlenie liczby orzeczeń w poszczególnych miesiącach (rozkład statystyczny)");
-        System.out.println("courts - wyświetlenie liczby orzeczeń ze względu na typ sądu (rozkład statystyczny)");
-        System.out.println("regulations - wyświetlenie 10 najczęściej przywoływanych ustaw");
-        System.out.println("jury - wyświetlenie liczby spraw przypadających na określony skład sędziowski (określoną liczbę sędziów)");
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
